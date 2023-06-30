d8750df319090f285bc172ea04ce34af1423eb34
MikeRalston98 <107587012+MikeRalston98@users.noreply.github.com>
2023-06-30 11:14:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,216 +1,31 @@
-// Check URL Hash for Login with Webex Token
-parseJwtFromURLHash();
 
-const app = new window.Webex.Application();
+// Create a new Webex app instance
+var app = new window.Webex.Application();
 
+// Wait for onReady() promise to fulfill before using framework
 app.onReady().then(() => {
-  log("onReady()", { message: "host app is ready" });
-
-  // Listen and emit any events from the EmbeddedAppSDK
-  app.listen().then(() => {
-    app.on("application:displayContextChanged", (payload) =>
-      log("application:displayContextChanged", payload)
-    );
-    app.on("application:shareStateChanged", (payload) =>
-      log("application:shareStateChanged", payload)
-    );
-    app.on("application:themeChanged", (payload) =>
-      log("application:themeChanged", payload)
-    );
-    app.on("meeting:infoChanged", (payload) =>
-      log("meeting:infoChanged", payload)
-    );
-    app.on("meeting:roleChanged", (payload) =>
-      log("meeting:roleChanged", payload)
-    );
-    app.on("space:infoChanged", (payload) => log("space:infoChanged", payload));
-  });
+    log("App ready. Instance", app);
+}).catch((errorcode) =>  {
+    log("Error with code: ", Webex.Application.ErrorCodes[errorcode])
 });
 
-/**
- * Sets the share url to the value entereed in the "shareUrl" element.
- * @returns
- */
+// Button click handler to set share URL
 function handleSetShare() {
-  if (app.isShared) {
-    log("ERROR: setShareUrl() should not be called while session is active");
-    return;
-  }
-  var url = document.getElementById("shareUrl").value;
-  app
-    .setShareUrl(url, url, "Embedded App Kitchen Sink")
-    .then(() => {
-      log("setShareUrl()", {
-        message: "shared url to participants panel",
-        url: url,
-      });
-    })
-    .catch((error) => {
-      log(
-        "setShareUrl() failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
+    // Replace this with the URL of your shared page
+    var url = "https://www.example.com/shared.html"
+    // "Shared App" is the title of the window or tab that will be created
+    app.setShareUrl(url, "", "Shared App").then(() => {
+        log("Set share URL", url);
+    }).catch((errorcode) => {
+        log("Error: ", Webex.Application.ErrorCodes[errorcode])
     });
 }
 
-/**
- * Clears the share url
- */
-function handleClearShare() {
-  app
-    .clearShareUrl()
-    .then(() => {
-      log("clearShareUrl()", { message: "share url has been cleared" });
-    })
-    .catch((error) => {
-      log(
-        "clearShareUrl() failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-/**
- * Sets the presentation URL
- */
-async function handleSetPresentationUrl() {
-  if (app.isShared) {
-    log("ERROR: setShareUrl() should not be called while session is active");
-    return;
-  }
-  var url = document.getElementById("shareUrl").value;
-  let meeting = await app.context.getMeeting();
-  meeting.setPresentationUrl(url, "My Presentation", Webex.Application.ShareOptimizationMode.AUTO_DETECT, false)
-    .then(() => {
-      log("setPresentationUrl()", {
-        message: "presented url to participants panel",
-        url: url,
-      });
-    })
-    .catch((error) => {
-      log(
-        "setPresentationUrl() failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-/**
- * Clears the set presentation url
- */
-async function handleClearPresentationUrl() {
-  let meeting = await app.context.getMeeting();
-  meeting.clearPresentationUrl()
-    .then(() => {
-      log("clearPresentationUrl()", {
-        message: "cleared url to participants panel",
-        url: url,
-      });
-    })
-    .catch((error) => {
-      log(
-        "clearPresentationUrl() failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-var embedded_app = new window.webex.Application();
-var sidebar;
-var callCount;
-var importantContactId = "(480) 561-9558";
-
-
-embedded_app.onReady().then(() => {
-    log("onReady()", { message: "EA is ready." });
-    embedded_app.listen().then(() => {
-      embedded_app.on("sidebar:callStateChanged", (call) => {
-        console.log("Call state changed. Call object:", call);
-        handleCallStateChange(call);
-      });
-      embedded_app.on("application:viewStateChanged", (viewState) => {
-        console.log("View state changed. Current view:", viewState);
-        switch (viewState) {
-          case "IN_FOCUS":
-            // User has noticed the badge and has responded, so we can remove it...
-            initializeSideBar(callCount++);
-            break;
-        }
-      });
-    });
-  });
-
-function handleCallStateChange(call) {
-  switch (call.state) {
-    case "Started":
-      console.log("A call has come in...");
-      
-      // Check to see if the call is from a VIP...
-      if (call.id === importantContactId) {
-        console.log("A VIP call is incoming! Notify the user...");
-        // Initialize the sidebar, passing in the incremented the badge count...
-        initializeSideBar(callCount++);
-      }
-      initializeSideBar(callCount++);
-
-      // For all calls, log the information...
-      console.log("*** CALL INFORMATION ***")
-      console.log("- Caller ID: ", call.id);
-      console.log("- Call type: ", call.callType);
-      console.log("- Call state: ", call.state);
-      console.log("- Local Participant: ", call.localParticipant);
-      console.log("- Remote Participants list: ", call.remoteParticpants);
-      break;
-    case "Connected":
-      console.log("Call is connected.");
-      break;
-    case "Ended":
-      console.log("Call is ended.");
-      break;
-    default:
-      break;
-  }
-}
-
-
-function initializeSideBar(callCount) {
-  embedded_app.context.getSidebar().then((s) => {
-      sidebar = s;
-      console.log("Show a badge on the sidebar...")
-      handleBadge(callCount, sidebar);
-    })
-    .catch((error) => {
-      console.log("getSidebar() failed. Error: ", Webex.Application.ErrorCodes[error]);
-    });
-}
-
-function handleBadge(callCount, sidebar) {
-  // Make sure the sidebar is available..
-  if (!sidebar) {
-    console.log("Sidebar info is not available. Error: ", Webex.Application.ErrorCodes[4]);
-    return;
-  }
-
-  // Initialize a badge object...
-  const badge = {
-    badgeType: 'count',
-    count: callCount,
-  };
-
-  // Show the badge...
-  sidebar.showBadge(badge).then((success) => {
-      console.log("sidebar.showBadge() successful.", success);
-    }).catch((error) => {
-      console.log("sidebar.showBadge() failed. Error: ", Webex.Application.ErrorCodes[error]);
-    });
+// Utility function to log app messages
+function log(type, data) {
+    var ul = document.getElementById("console");
+    var li = document.createElement("li");
+    var payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
+    li.appendChild(payload)
+    ul.prepend(li);
 }

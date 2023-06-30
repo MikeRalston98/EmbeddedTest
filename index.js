3b8bfe56ba51c0ce21227511282d7bedb21e78cb
MikeRalston98 <107587012+MikeRalston98@users.noreply.github.com>
2023-06-30 11:57:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Check URL Hash for Login with Webex Token
+// Check URL Hash for login with Webex Token
 parseJwtFromURLHash();
 
 //const app = new window.Webex.Application();
@@ -6,18 +6,18 @@ const app = new webex.Application();
 //await app.onReady();
 const sidebar = await app.context.getSidebar();
 app.onReady().then(() => {
-    log("onReady()", { message: "EA is ready." });
-    log("onReady()", { message: "EA is reeadyyy." });
+    console.log("onReady()", { message: "EA is ready." });
+    console.log("onReady()", { message: "EA is reeadyyy." });
 
     app.listen().then(() => {
       app.on("sidebar:callStateChanged", (call) => {
-        log("Call state changed. Call object:", call);
-        log("Call state changed. Call object:", { message: call });
+        console.log("Call state changed. Call object:", call);
+        console.log("Call state changed. Call object:", { message: call });
 
         handleCallStateChange(call);
       });
       app.on("application:viewStateChanged", (viewState) => {
-        log("View state changed. Current view:", viewState);
+        console.log("View state changed. Current view:", viewState);
         switch (viewState) {
           case "IN_FOCUS":
             // User has noticed the badge and has responded, so we can remove it...
@@ -31,28 +31,28 @@ app.onReady().then(() => {
 function handleCallStateChange(call) {
   switch (call.state) {
     case "Started":
-      log("A call has come in...");
+      console.log("A call has come in...");
       
       // Check to see if the call is from a VIP...
       if (call.id === importantContactId) {
-        log("A VIP call is incoming! Notify the user...");
+        console.log("A VIP call is incoming! Notify the user...");
         // Initialize the sidebar, passing in the incremented the badge count...
         initializeSideBar(callCount++);
       }
       
-      // For all calls, log the information...
-      log("*** CALL INFORMATION ***")
-      log("- Caller ID: ", call.id);
-      log("- Call type: ", call.callType);
-      log("- Call state: ", call.state);
-      log("- Local Participant: ", call.localParticipant);
-      log("- Remote Participants list: ", call.remoteParticpants);
+      // For all calls, console.log the information...
+      console.log("*** CALL INFORMATION ***")
+      console.log("- Caller ID: ", call.id);
+      console.log("- Call type: ", call.callType);
+      console.log("- Call state: ", call.state);
+      console.log("- Local Participant: ", call.localParticipant);
+      console.log("- Remote Participants list: ", call.remoteParticpants);
       break;
     case "Connected":
-      log("Call is connected.");
+      console.log("Call is connected.");
       break;
     case "Ended":
-      log("Call is ended.");
+      console.log("Call is ended.");
       break;
     default:
       break;
@@ -62,18 +62,18 @@ function handleCallStateChange(call) {
 function initializeSideBar(callCount) {
   app.context.getSidebar().then((s) => {
       sidebar = s;
-      log("Show a badge on the sidebar...")
+      console.log("Show a badge on the sidebar...")
       handleBadge(callCount, sidebar);
     })
     .catch((error) => {
-      log("getSidebar() failed. Error: ", Webex.Application.ErrorCodes[error]);
+      console.log("getSidebar() failed. Error: ", Webex.Application.ErrorCodes[error]);
     });
 }
 
 function handleBadge(callCount, sidebar) {
   // Make sure the sidebar is available..
   if (!sidebar) {
-    log("Sidebar info is not available. Error: ", Webex.Application.ErrorCodes[4]);
+    console.log("Sidebar info is not available. Error: ", Webex.Application.ErrorCodes[4]);
     return;
   }
 
@@ -85,33 +85,33 @@ function handleBadge(callCount, sidebar) {
 
   // Show the badge...
   sidebar.showBadge(badge).then((success) => {
-      log("sidebar.showBadge() successful.", success);
+      console.log("sidebar.showBadge() successful.", success);
     }).catch((error) => {
-      log("sidebar.showBadge() failed. Error: ", Webex.Application.ErrorCodes[error]);
+      console.log("sidebar.showBadge() failed. Error: ", Webex.Application.ErrorCodes[error]);
     });
 }
 
 app.onReady().then(() => {
-  log("onReady()", { message: "host app is ready" });
+  console.log("onReady()", { message: "host app is ready" });
 
   // Listen and emit any events from the EmbeddedAppSDK
   app.listen().then(() => {
     app.on("application:displayContextChanged", (payload) =>
-      log("application:displayContextChanged", payload)
+      console.log("application:displayContextChanged", payload)
     );
     app.on("application:shareStateChanged", (payload) =>
-      log("application:shareStateChanged", payload)
+      console.log("application:shareStateChanged", payload)
     );
     app.on("application:themeChanged", (payload) =>
-      log("application:themeChanged", payload)
+      console.log("application:themeChanged", payload)
     );
     app.on("meeting:infoChanged", (payload) =>
-      log("meeting:infoChanged", payload)
+      console.log("meeting:infoChanged", payload)
     );
     app.on("meeting:roleChanged", (payload) =>
-      log("meeting:roleChanged", payload)
+      console.log("meeting:roleChanged", payload)
     );
-    app.on("space:infoChanged", (payload) => log("space:infoChanged", payload));
+    app.on("space:infoChanged", (payload) => console.log("space:infoChanged", payload));
   });
 });
 
@@ -121,20 +121,20 @@ app.onReady().then(() => {
  */
 function handleSetShare() {
   if (app.isShared) {
-    log("ERROR: setShareUrl() should not be called while session is active");
+    console.log("ERROR: setShareUrl() should not be called while session is active");
     return;
   }
   var url = document.getElementById("shareUrl").value;
   app
     .setShareUrl(url, url, "Embedded App Kitchen Sink")
     .then(() => {
-      log("setShareUrl()", {
+      console.log("setShareUrl()", {
         message: "shared url to participants panel",
         url: url,
       });
     })
     .catch((error) => {
-      log(
+      console.log(
         "setShareUrl() failed with error",
         Webex.Application.ErrorCodes[error]
       );
@@ -148,10 +148,10 @@ function handleClearShare() {
   app
     .clearShareUrl()
     .then(() => {
-      log("clearShareUrl()", { message: "share url has been cleared" });
+      console.log("clearShareUrl()", { message: "share url has been cleared" });
     })
     .catch((error) => {
-      log(
+      console.log(
         "clearShareUrl() failed with error",
         Webex.Application.ErrorCodes[error]
       );
@@ -163,20 +163,20 @@ function handleClearShare() {
  */
 async function handleSetPresentationUrl() {
   if (app.isShared) {
-    log("ERROR: setShareUrl() should not be called while session is active");
+    console.log("ERROR: setShareUrl() should not be called while session is active");
     return;
   }
   var url = document.getElementById("shareUrl").value;
   let meeting = await app.context.getMeeting();
   meeting.setPresentationUrl(url, "My Presentation", Webex.Application.ShareOptimizationMode.AUTO_DETECT, false)
     .then(() => {
-      log("setPresentationUrl()", {
+      console.log("setPresentationUrl()", {
         message: "presented url to participants panel",
         url: url,
       });
     })
     .catch((error) => {
-      log(
+      console.log(
         "setPresentationUrl() failed with error",
         Webex.Application.ErrorCodes[error]
       );
@@ -190,13 +190,13 @@ async function handleClearPresentationUrl() {
   let meeting = await app.context.getMeeting();
   meeting.clearPresentationUrl()
     .then(() => {
-      log("clearPresentationUrl()", {
+      console.log("clearPresentationUrl()", {
         message: "cleared url to participants panel",
         url: url,
       });
     })
     .catch((error) => {
-      log(
+      console.log(
         "clearPresentationUrl() failed with error",
         Webex.Application.ErrorCodes[error]
       );
